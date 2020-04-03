d60d819f4655f261863b6de8a51c84d2b222e65a
Juan Pablo <juanpaps03@gmail.com>
2020-04-03 20:31:23 -0300

    agregue los getters and setters

--- usuario.java
+++ usuario.java
@@ -1,4 +1,20 @@
 public class usuario {
     String cedula;
     int edad;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
